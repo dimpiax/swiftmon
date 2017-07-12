2b17d73ce267c78135b6caa53bbcb6f32a6b7050
Pilipenko Dima <dimpiax@gmail.com>
2017-07-12 16:59:26 +0300

    add basic functionality

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 /* @flow */
 
-console.log('pussy')
+import path from 'path'
+
+import Daemon from './src/managers/daemon'
+import config from './src/config'
+
+const daemon = new Daemon()
+
+const start = () => {
+    // constants
+    const env = 'debug'
+    const startScript = path.parse(process.cwd()).base
+
+    daemon.options = { buildDir: config.args.buildDir, env, startScript }
+    daemon.start()
+}
+
+module.exports = {
+    start
+}
